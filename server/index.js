59afef7822ce3ff9be8a0f3bace53da0a4593beb
Chandu-Develop <bhuvanchandumidde3@gmail.com>
2024-05-12 21:46:28 +0530

    comm

--- server/index.js
+++ server/index.js
@@ -1,60 +1,59 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/countrycapitals')
-    .then(() => {
-        console.log('mongodb connected');
+mongoose
+  .connect('mongodb://localhost:27017/countrycapitals')
+  .then(() => {
+    console.log('mongodb connected');
 
-        app.listen(3030, () => {
-            console.log(`serving at port 3030`)
-        })
-    })
-    .catch((e) => {
-        console.error(`MongoDB error: ${e.message}`);
-    })
-
+    app.listen(3030, () => {
+      console.log(`serving at port 3030`);
+    });
+  })
+  .catch((e) => {
+    console.error(`MongoDB error: ${e.message}`);
+  });
 
 const mysql = require('mysql2');
 
 const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: process.env.mysqlPassword,
-    database: process.env.mysqlDB
-})
+  host: 'localhost',
+  user: 'root',
+  password: process.env.mysqlPassword,
+  database: process.env.mysqlDB,
+});
 
 conn.connect((err) => {
-    if (err) {
-        console.error(`MySQL error: ${err.message}`);
-        return;
-    } else {
-        console.log(`mysql DB connected`);
+  if (err) {
+    console.error(`MySQL error: ${err.message}`);
+    return;
+  } else {
+    console.log(`mysql DB connected`);
 
-        // app.listen(3030, () => console.log(`serving at port 3030`))
-    }
-})
+    // app.listen(3030, () => console.log(`serving at port 3030`))
+  }
+});
 
-const countryModel = require('./mongoModel')
-app.get('/', async(req, res)=>{
-    try {
-        const result = await countryModel.find()
-        res.json({length:result.length, payload:result})
-    } catch (e) {
-        console.error(`DB error: ${e.message}`);
-        res.status(404);
-    } 
-})
+const countryModel = require('./mongoModel');
+app.get('/', async (req, res) => {
+  try {
+    res.send('this is from the server');
+  } catch (e) {
+    console.error(`DB error: ${e.message}`);
+    res.status(404);
+  }
+});
 app.get('/:language', async (req, res) => {
-    try {
-        const language = req.params?.language
-        const result = await countryModel.find({language:language})
-        res.json(result)
-    } catch (e) {
-        console.error(`DB error: ${e.message}`);
-        res.status(404);
-    }
-})
+  try {
+    const language = req.params?.language;
+    const result = await countryModel.find({ language: language });
+    res.json(result);
+  } catch (e) {
+    console.error(`DB error: ${e.message}`);
+    res.status(404);
+  }
+});
